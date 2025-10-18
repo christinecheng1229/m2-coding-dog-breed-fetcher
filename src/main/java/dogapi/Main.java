4a8d832c47a29fb80331adbf91db8d24cc617985
Christine <christinecheng1229@gmail.com>
2025-10-18 15:05:28 -0400

    added try-catch in main

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -5,14 +5,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String breed = "hound";
-        BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
-        int result = getNumberOfSubBreeds(breed, breedFetcher);
-        System.out.println(breed + " has " + result + " sub breeds");
+        try {
+            String breed = "hound";
+            BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
+            int result = getNumberOfSubBreeds(breed, breedFetcher);
+            System.out.println(breed + " has " + result + " sub breeds");
 
-        breed = "cat";
-        result = getNumberOfSubBreeds(breed, breedFetcher);
-        System.out.println(breed + " has " + result + " sub breeds");
+            breed = "cat";
+            result = getNumberOfSubBreeds(breed, breedFetcher);
+            System.out.println(breed + " has " + result + " sub breeds");
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
@@ -23,7 +27,7 @@ public class Main {
      * @return the number of sub breeds. Zero should be returned if there are no sub breeds
      * returned by the fetcher
      */
-    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
+    public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) throws BreedFetcher.BreedNotFoundException {
         // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
         return -1;
